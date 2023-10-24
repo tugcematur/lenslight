3c135b5a0a440565b8c6e2712a98895e2cb98038
tugcematur <39506647+tugcematur@users.noreply.github.com>
2023-10-24 12:14:23 +0300

    update

---
+++ app.js
@@ -0,0 +1,13 @@
+import express from "express"
+
+
+const app = express()
+const port = 3000
+
+app.get('/',(req,res)=>{
+    res.send("Index")
+})
+
+app.listen(port,() => {
+    console.log(`server calisiyor, ${port}`)
+})
